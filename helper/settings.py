beb67c4c0edfb5c75ef4c980b806e9573fd84b50
Francis Tsang <frtsang40@gmail.com>
2021-10-11 22:55:10 -0700

    create first model

--- helper/settings.py
+++ helper/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'list.apps.ListConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- list/models.py
+++ list/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Listings(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    # price = models.DecimalField(decimal_places=2, max_digits=10000)
+    summary = models.TextField()
+    # author = models.ForeignKey(User, on_delete=models.PROTECT)
+    # slug = models.CharField(max_length=settings.POST_TITLE_MAX_LENGTH, blank=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+    # def save(self, *args, **kwargs):
+    #     if not self.pk:
+    #         self.slug = slugify(self.title, allow_unicode=True)
+    #     return super(Products, self).save(*args, **kwargs)
+
+    # def get_absolute_url(self):
+    #     """ Returns a fully-qualified path for a page"""
+    #     path_components = {'slug': self.slug}
+    #     return reverse('store-item', kwargs=path_components)    
