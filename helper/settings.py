28d2a58dc2e37e089521af0ccbcf0b8a482bef42
Francis Tsang <frtsang40@gmail.com>
2021-10-15 10:31:52 -0700

    misnamed allowed host site should work now

--- helper/settings.py
+++ helper/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = ['localhost', 'uniqueprojectname.herokuapp.com']
+ALLOWED_HOSTS = ALLOWED_HOSTS = ['localhost', 'helperlist.herokuapp.com']
 
 
 # Application definition
